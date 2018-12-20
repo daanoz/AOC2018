7e90386217ecb1ba7ec517858d44b42ace84b942
Daan Sieben <developer@daansieben.net>
2018-12-20 13:03:56 +0100

    Day 20

--- 20/index.js
+++ 20/index.js
@@ -1,17 +1,11 @@
 const term = require('terminal-kit').terminal;
 const utils = require('./../utils');
-const ScreenBuffer = require('terminal-kit').ScreenBuffer;
 
 class Cell {
-    constructor(x, y) {
+    constructor(x, y, distance) {
         this.x = x;
         this.y = y;
-        this.sides = {
-            N: null,
-            E: null,
-            S: null,
-            W: null
-        };
+        this.distance = distance;
     }
     coordThatIs(d) {
         switch(d) {
@@ -22,132 +16,50 @@ class Cell {
             default: { throw new Error('Unknown direction: ' + d); }
         }
     }
-    link(cell, d) {
-        switch(d) {
-            case 'W' : this.sides.W = cell; cell.sides.E = this; break;
-            case 'S' : this.sides.S = cell; cell.sides.N = this; break;
-            case 'E' : this.sides.E = cell; cell.sides.W = this; break;
-            case 'N' : this.sides.N = cell; cell.sides.S = this; break;
-        }
-    }
-}
-
-let lastT = 0;
-let createdCells = 0;
-let existingCells = 0;
-let endings = 0;
-const findLocations = (map, grid, cur, path) => {
-    let now = new Date().getTime();
-    if(now - lastT > 5000) {
-        //draw(grid);
-        console.log(createdCells, existingCells, endings);
-        lastT = now;
-    }
-    let next = map.shift();
-    if(next === '$') { // we are done here!
-        endings++;
-        return [path];
-    } else if(next === '(') { // we can go multiple directions here...
-        let endPos = -1;
-        let depth = 1;
-        let c = 0;
-        let sections = [];
-        let nextSection = 0;
-        while(endPos < 0 && c < map.length) {
-            if(map[c] === '(') {
-                depth++;
-            } else if(map[c] === ')') {
-                depth--;
-                if(depth <= 0) {
-                    endPos = c;
-                    sections.push(map.slice(nextSection, c));
-                }
-            } else if(map[c] === '|' && depth === 1) {
-                sections.push(map.slice(nextSection, c));
-                nextSection = c + 1;
-            }
-            c++;
-        }
-        if(endPos < 0) { throw new Error('Broken regex found!'); }
-        map.splice(0, endPos + 1);
-        return sections
-            .map(s => findLocations(s.concat(map), grid, cur, path.concat([])))
-            .reduce((list, s) => list.concat(s), []);
-    } else {
-        let [newX, newY] = cur.coordThatIs(next);
-        if(!grid[newY]) {
-            grid[newY] = {};
-            grid[newY][newX] = new Cell(newX, newY);
-            createdCells++;
-        } else if(!grid[newY][newX]) {
-            grid[newY][newX] = new Cell(newX, newY);
-            createdCells++;
-        } else {
-            existingCells++;
-        }
-        let nextCell = grid[newY][newX];
-        cur.link(nextCell, next);
-        let cid = `${newX}:${newY}`;
-        let existingCid = path.indexOf(cid);
-        if(existingCid >= 0) {
-            path = path.slice(0, existingCid + 1);
-        } else {
-            path.push(cid);
-        }
-        return findLocations(map, grid, nextCell, path);
-    }
-};
-
-const draw = (grid) => {
-    let range =[[Number.MAX_VALUE, Number.MIN_VALUE],[Number.MAX_VALUE, Number.MIN_VALUE]];
-    Object.keys(grid).forEach(_y => {
-        let y = parseInt(_y, 10);
-        range[1][0] = Math.min(range[1][0], y);
-        range[1][1] = Math.max(range[1][1], y);
-        Object.keys(grid[_y]).forEach((_x) => {
-            let x = parseInt(_x, 10);
-            range[0][0] = Math.min(range[0][0], x);
-            range[0][1] = Math.max(range[0][1], x);
-        })
-    });
-    term.clear();
-    screen = new ScreenBuffer({
-        dst: term,
-        width: ((range[0][1] - range[0][0]) * 2) + 3,
-        height: ((range[1][1] - range[1][0]) * 2) + 20,
-    });
-    screen.fill({ char: '#' });
-    for(let y = range[1][0]; y <= range[1][1]; y++) {
-        for(let x = range[0][0]; x <= range[0][1]; x++) {
-            let scrnX = ((x - range[0][0]) * 2) + 1;
-            let scrny = ((y - range[1][0]) * 2) + 1;
-            let cell = grid[y][x];
-            if(cell) {
-                screen.put({ x: scrnX, y: scrny }, x === 0 && y === 0 ? 'X' : '.');
-                if(cell.sides.S) {
-                    screen.put({ x: scrnX, y: scrny + 1 }, '-');
-                }
-                if(cell.sides.E) {
-                    screen.put({ x: scrnX + 1, y: scrny }, '|');
-                }
-            }
-        }
-    }
-    screen.draw();
 }
 
 module.exports = (isPartB) => {
     let input = utils.readInput('').split('');
-    input.shift(); // ^
+    input.shift();// ^
 
-    let grid = {'0' : {'0' : new Cell(0, 0)}};
-    let allPaths = findLocations(input, grid, grid[0][0], ['0:0']);
+    let grid = {'0' : {'0' : new Cell(0, 0, 0)}};
 
-    //draw(grid);
+    let splitPoints = [];
+    let nextPos = grid[0][0];
+    input.forEach(next => {
+        if(next === '$') { // we are done here!
+            return;
+        } else if(next === '(') {
+            splitPoints.push(nextPos);
+        } else if(next === ')') {
+            nextPos = splitPoints.pop();
+        } else if(next === '|') {
+            nextPos = splitPoints[splitPoints.length - 1];
+        } else {
+            let [newX, newY] = nextPos.coordThatIs(next);
+            if(!grid[newY]) {
+                grid[newY] = {};
+                grid[newY][newX] = new Cell(newX, newY, nextPos.distance + 1);
+            } else if(!grid[newY][newX]) {
+                grid[newY][newX] = new Cell(newX, newY, nextPos.distance + 1);
+            } else {
+                grid[newY][newX].distance = Math.min(nextPos.distance + 1, grid[newY][newX].distance);
+            }
+            nextPos = grid[newY][newX];
+        }
+    });
 
     if(!isPartB)  {
-        term.bold('Result for part A: %s\n', 0);
+        let largest = Object.values(grid).reduce((highestRow, row) => {
+            let rowHigh = Object.values(row).reduce((highest, cell) => cell.distance > highest ? cell.distance : highest, 0);
+            return rowHigh > highestRow ? rowHigh : highestRow;
+        }, 0);
+        term.bold('Result for part A: %s\n', largest);
     } else {
-        term.bold('Result for part B: %s\n', 0);
+        let thousandRooms = Object.values(grid).reduce((list, row) => {
+            let roomRowList = Object.values(row).filter(cell => cell.distance >= 1000);
+            return list.concat(roomRowList);
+        }, []);
+        term.bold('Result for part B: %s\n', thousandRooms.length);
     }
 };
